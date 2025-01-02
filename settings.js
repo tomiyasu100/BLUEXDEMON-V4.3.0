6a3c33f41a66e66c0573cabfbb8fb8d953409819
tomiyasu100 <bakaretomisin235@gmail.com>
2025-01-02 17:19:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2347071701141"
 
 global.botNumber = ["2349019529423"]
 global.domain = "https://.nobodysey.me"
